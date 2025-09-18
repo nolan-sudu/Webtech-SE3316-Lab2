c851ed07ec7498062ccacec81bfa185ef6997fe9
Nolan <nsuduwel@uwo.ca>
2025-09-18 15:13:53 -0400

    Added page structure with header, search box, dropdown, list, footer, and basic CSS styling

---
+++ lab1.js
@@ -0,0 +1,7 @@
+function searchButterflies() {
+    alert("Search function will be implemented here.");
+}
+
+function showButterflyInfo() {
+    alert("Dropdown function will be implemented here.");
+}
